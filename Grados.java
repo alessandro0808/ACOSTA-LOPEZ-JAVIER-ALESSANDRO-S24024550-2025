89ca92bd9e165490f6ca9bd711d189570d286c21
alessandro0808 <alessandro20470@gmail.com>
2025-10-23 23:37:15 -0600

    Add files via upload

---
+++ Grados.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Grados {
+	public static void main(String args[]){	
+	Scanner sc = new Scanner(System.in);
+	double a=0, d=0, b=0;
+	System.out.println("Ingrese los grados centigrados que se encuentran");
+	a =sc.nextDouble();
+	b=c+273.15;
+	d=(c*1.8)+32;
+	System.out.println("Los grados C a Fahrenheit son: " +d+ "\nY en temperatura absoluta son: "+b );
+}
+}
